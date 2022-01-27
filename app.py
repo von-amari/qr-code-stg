ab5f5d00f52692aac15e12f434fe6dc4562fc25d
Yvonne K <60331179+von-amari@users.noreply.github.com>
2022-01-26 19:54:45 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,34 +1,50 @@
-from io import BytesIO
-from io import StringIO
-from flask import Flask, request, send_file
-from flask_cors import CORS
-import qrcode
-import barcode
-from io import BytesIO
-
-from flask import make_response, url_for, redirect
-from flask import request, render_template, flash
-import barcode
-import qrcode
-
-
+# app.py
+from flask import Flask, request, jsonify
 app = Flask(__name__)
-CORS(app)
 
-def gen_code39(content):
-    fp = StringIO()
-    makepng = barcode.writer.ImageWriter()
-    barcode.generate("Code39", content, output=fp, writer=makepng)
-    image = fp.getvalue()
-    fp.close()
-    return image
+@app.route('/getmsg/', methods=['GET'])
+def respond():
+    # Retrieve the name from url parameter
+    phone = request.args.get("phone", None)
 
+    # For debugging
+    print(f"got phone {phone}")
 
-@app.route('/generate_qrcode/<uuid>', methods=['POST'])
-def render_code39(uuid):
-    response = make_response(gen_code39(uuid))
-    response.headers["Content-type"] = "image/png"
-    return response
+    response = {}
 
+    # Check if user sent a name at all
+    if not phone:
+        response["ERROR"] = "no phone found, please send a phone."
+    # Check if the user entered a number not a name
 
-app.run()
+    # Now the user entered a valid name
+    else:
+        response["MESSAGE"] = f"Your phone is {phone}"
+
+    # Return the response in json format
+    return jsonify(response)
+
+@app.route('/post/', methods=['POST'])
+def post_something():
+    param = request.form.get('phone')
+    print(param)
+    # You can add the test cases you made in the previous function, but in our case here you are just testing the POST functionality
+    if param:
+        return jsonify({
+            "Message": f"Phone is {phone}",
+            # Add this option to distinct the POST request
+            "METHOD" : "POST"
+        })
+    else:
+        return jsonify({
+            "ERROR": "no phone found, please send a phone."
+        })
+
+# A welcome message to test our server
+@app.route('/')
+def index():
+    return "<h1>Welcome to our server !!</h1>"
+
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
