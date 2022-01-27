6f9e0a88400d5ee4dff8d472fd0ac2531c58d1ef
Yvonne K <60331179+von-amari@users.noreply.github.com>
2022-01-26 20:56:10 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,50 +1,25 @@
 # app.py
-from flask import Flask, request, jsonify
+from flask import Flask, request, send_file
+import qrcode
+from flask_cors import CORS
+from io import BytesIO
+
 app = Flask(__name__)
+CORS(app)
 
-@app.route('/getmsg/', methods=['GET'])
-def respond():
+@app.route('/generage_qrcode', methods=['POST'])
+def generate_qrcode():
     # Retrieve the name from url parameter
-    phone = request.args.get("phone", None)
+    buffer = BytesIO()
+    data = request.form.get('data')
 
-    # For debugging
-    print(f"got phone {phone}")
+    img = qrcode.make(data)
+    img.save(buffer)
+    buffer.seek(0)
+    response = send_file(buffer, mimetype='image/png')
+    return response
 
-    response = {}
-
-    # Check if user sent a name at all
-    if not phone:
-        response["ERROR"] = "no phone found, please send a phone."
-    # Check if the user entered a number not a name
-
-    # Now the user entered a valid name
-    else:
-        response["MESSAGE"] = f"Your phone is {phone}"
-
-    # Return the response in json format
-    return jsonify(response)
-
-@app.route('/post/', methods=['POST'])
-def post_something():
-    param = request.form.get('phone')
-    print(param)
-    # You can add the test cases you made in the previous function, but in our case here you are just testing the POST functionality
-    if param:
-        return jsonify({
-            "Message": f"Phone is {phone}",
-            # Add this option to distinct the POST request
-            "METHOD" : "POST"
-        })
-    else:
-        return jsonify({
-            "ERROR": "no phone found, please send a phone."
-        })
-
-# A welcome message to test our server
-@app.route('/')
-def index():
-    return "<h1>Welcome to our server !!</h1>"
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
-    app.run(threaded=True, port=5000)
+    app.run()
